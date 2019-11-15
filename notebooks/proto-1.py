b54280b2ecf69122e4af03745b8727ae0605e764
uncojohnco <qaza7532>
2019-11-15 14:33:32 -0500

    notebooks - add proto-1 nb

---
+++ notebooks/proto-1.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+
+# %% {"pycharm": {"is_executing": false}, "jupyter": {"outputs_hidden": true}}
+files_pre = """
+alpha.txt
+file01_0040.rgb
+file01_0041.rgb
+file01_0042.rgb
+file01_0043.rgb
+file02_0044.rgb
+file02_0045.rgb
+file02_0046.rgb
+file02_0047.rgb
+file1.03.rgb
+file2.03.rgb
+file3.03.rgb
+file4.03.rgb
+file.info.03.rgb
+"""
+
+
+# %%
+files = files_pre.split()
+
+# %%
