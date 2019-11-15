437f1723f8a2a5e38a58b8dd7ca965116596096b
uncojohnco <qaza7532>
2019-11-15 16:44:30 -0500

    yield_file_goups.py	notebooks - latest

--- notebooks/yield_file_goups.py
+++ notebooks/yield_file_goups.py
@@ -14,5 +14,43 @@
 # ---
 
 # %% {"jupyter": {"outputs_hidden": true}}
+from typing import Dict, Pattern, Tuple
 
+from dataclasses import dataclass
+
+from IPython.display import display
+
+
+# %% {"pycharm": {"name": "#%%\n"}, "jupyter": {"outputs_hidden": false}}
+@dataclass
+class FileData:
+    
+    dir: str
+    basename: str
+    extension: str
+
+
+# %% {"pycharm": {"name": "#%%\n"}, "jupyter": {"outputs_hidden": false}}
+
+def yield_file_groups(file_seq_map):
+    
+    for (dirname, basename, ext), file_paths in file_seq_map.items():
+        
+        fd = FileData(dirname, basename, ext)
+        
+        if file_paths > 1:
+            yield Fileobj(file_data=fd)
+        else:
+            yield FileSequence(file_data=fd, file_paths=file_paths)
+            
+
+
+# %% {"pycharm": {"name": "#%%\n"}, "jupyter": {"outputs_hidden": false}}
+    file_seq_map = {
+        
+    }
+
+# %% {"pycharm": {"name": "#%%\n"}, "jupyter": {"outputs_hidden": false}}
+    file_groups = yield_file_groups(file_seq_map)
+    file_groups
 
