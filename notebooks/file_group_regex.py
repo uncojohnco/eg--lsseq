b5d845596282697fe632f13214f7b5cb06d281eb
uncojohnco <qaza7532>
2019-11-15 16:44:23 -0500

    file_group_regex.ipynb	notebooks - latest

--- notebooks/file_group_regex.py
+++ notebooks/file_group_regex.py
@@ -13,8 +13,16 @@
 #     name: python3
 # ---
 
+# %% {"pycharm": {"name": "#%%\n", "is_executing": false}, "jupyter": {"outputs_hidden": false}}
+from typing import Dict, Pattern, Tuple
 
-# %% {"pycharm": {"is_executing": false}, "jupyter": {"outputs_hidden": true}}
+import re
+
+from collections import defaultdict
+
+from IPython.display import display
+
+# %% {"jupyter": {"outputs_hidden": true}, "pycharm": {"is_executing": false}}
 files_pre = """
 alpha.txt
 file01_0040.rgb
@@ -32,8 +40,47 @@ file4.03.rgb
 file.info.03.rgb
 """
 
-
-# %%
 files = files_pre.split()
+display(files)
 
-# %%
+
+# %% {"pycharm": {"name": "#%%\n", "is_executing": false}, "jupyter": {"outputs_hidden": false}}
+# Regular expression pattern for matching file names on disk.
+FILE_GROUP_PATTERN = r"^(?P<basename>.*?)(?P<frames>\d+)?(?P<ext>\.\w*)*?$"
+FILE_GROUP_RE = re.compile(FILE_GROUP_PATTERN)
+
+# TODO: For now we do the work on the file name 
+#  and not the full path to keep the complexity minimal...
+def get_file_group_key(filename, disk_re: Pattern = None) -> Dict:
+    """
+    >>> get_file_group_key('file01_0040.rgb')
+    {'basename': 'file01_', 'frames': '0040', 'ext': '.rgb'}
+    >>> get_file_group_key('file.info.03.rgb.txt')
+    {'basename': 'file.info.03', 'frames': 'None, 'ext': '.txt'}
+    
+    """
+    check =  FILE_GROUP_RE.match or disk_re
+    
+    match = check(filename)
+    groupdict = match.groupdict()
+    
+    return groupdict
+
+
+
+# %% {"pycharm": {"name": "#%%\n", "is_executing": false}, "jupyter": {"outputs_hidden": false}}
+# TODO: fix this edge case
+
+get_file_group_key('file.info.03.txt')
+# Should return...
+# {'basename': 'file.info.03', 'frames': None, 'ext': '.txt'}
+
+# %% {"pycharm": {"name": "#%%\n", "is_executing": false}, "jupyter": {"outputs_hidden": false}}
+# display(files)
+seqs = defaultdict(set)
+
+for fp in files:
+    groups = get_file_group_key(fp)
+    # key = (basename, ext)
+
+display(seqs)
