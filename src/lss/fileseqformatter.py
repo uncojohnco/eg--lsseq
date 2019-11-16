e0ff38bac4dc40ce4b9f33032f85e59cf8d8189a
uncojohnco <qaza7532>
2019-11-15 19:21:55 -0500

    fileseqformatter.py	src/lss

--- src/lss/fileseqformatter.py
+++ src/lss/fileseqformatter.py
@@ -1,15 +1,25 @@
-
-
 #!/usr/bin/env python3
+
 
 from typing import List
 
 from lss.dataclass import FileSequence, Fileobj
 
 
+# TODO: ...
+def format_frames_compact(frames):
+    """
+    Converts a list of numbers into the compact representation of the numbers.
 
-def compress_frames():
-    pass
+    >>> format_frames_compact([1, 2, 3, 4])
+    '1-4'
+    >>> format_frames_compact([1, 2, 5, 3])
+    '1-3 5'compress_frames
+    >>> format_frames_compact([1, 2, 5, 7, 8, 9])
+    '1-2 5 7-9'
+    """
+
+    return '1-2 5 7-9'
     
 
 class FileSeqFormatter:
@@ -17,36 +27,39 @@ class FileSeqFormatter:
     def __init__(self, file_groups: List[Fileobj]):
         self._file_groups = file_groups
 
-    def _file_sequence(self, fileobj: FileSequence):
+    @staticmethod
+    def _file_sequence(fileseq: FileSequence) -> str:
 
-        count = len(fileobj.frames)
+        count = len(fileseq.frames)
 
-        bn = fileobj.basename
+        bn = fileseq.basename
 
-        zfill = fileobj.zfill
+        zfill = fileseq.zfill
         pad_str = '%d'.format(zfill)
 
         if zfill > 1:
             pad_str = '%0{}d'.format(zfill)
 
-        ext = fileobj.extension
+        ext = fileseq.extension
 
-        frame_chunks = 
+        frames = fileseq.frames
+        frame_chunks = format_frames_compact(frames)
 
         output = f'{count} {bn}{pad_str}.{ext}\t{frame_chunks}'
 
+        return output
 
-    def _file_single(self, fileobj: Fileobj):
-        pass
+    @staticmethod
+    def _file_single(fileobj: Fileobj) -> str:
 
-    def _process(self, file_group: Fileobj):
+        bn = fileobj.basename
 
-        if len(file_group):
-            yield self._file_sequence(fg)
-        else:
-            yield self._file_single(fg)
+        return f'1 {bn}'
 
-    def do_it(self) -> str:
+    def do_it(self) -> List[str]:
 
         for fg in self._file_groups:
-            yield self._process(fg)
+            if isinstance(fg, FileSequence):
+                yield self._file_sequence(fg)
+            else:
+                yield self._file_single(fg)
