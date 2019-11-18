c093d335f608e187feaeb1bd4d20cd6fff8bd3dd
uncojohnco <johnny.p.cochrane@gmail.com>
2019-11-18 14:26:41 -0500

    lsseq.py	src/lss - add build_sequences_concrete - format_sequence - add docstring test

--- src/lss/lsseq.py
+++ src/lss/lsseq.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 
-from typing import Iterator, List, Iterable
+from typing import Iterator, List, Iterable, Generator
 
 from lss import FileItem, FileSequence
 from lss.builder import FileSequenceBuilder
@@ -37,56 +37,74 @@ def build_sequences_form1(file_items: Iterator[FileItem]) -> List[FileSequenceBu
     return sequences_f1
 
 
-def get_sequences(file_paths: Iterable[str]) -> List[FileSequence]:
+def build_sequences_concrete(sequences_f1: Iterable[FileSequenceBuilder]) -> Generator[FileSequenceBuilder, None, None]:
+
+    for seq_f1 in sequences_f1:
+
+        seq = FileSequence(
+            str_parts=seq_f1.seq_str_parts,
+            frames=seq_f1.ordered,
+            fileobj=seq_f1.fileobj,
+        )
+
+        yield seq
+
+
+def get_sequences(file_paths: Iterable[str]) -> Generator[FileSequence, None, None]:
     """
     Process a list of filenames to be collected into their
 
     Examples:
         >>> files = ['f01.rgb', 'f02.rgb','f03.rgb',]
-        >>> get_sequences(files)
-        [FileSequence(str_parts=SequenceStrParts(prefix='f', suffix='.rgb', pad_len=2, pad_char='#'), frames=(1, 2, 3), fileobj=Fileobj(dirname='.', ext='.rgb'))]
+        >>> list(get_sequences(files))
+        [FileSequence(str_parts=SequenceStrParts(prefix='f', suffix='.rgb', pad_len=2, pad_char='#'), fileobj=Fileobj(dirname='.', ext='.rgb'), frames=(1, 2, 3))]
 
     :param file_paths:
     :return:
     """
 
     file_items = map(FileItem, file_paths)
-
     sequences_f1 = build_sequences_form1(file_items)
-
-    sequences = []
-    for seq_f1 in sequences_f1:
-
-        ordered_frames = sorted(seq_f1.frames)
-
-        # TODO:
-        # if not seq_f1.is_sequence:
-        #   Handle single File ...
-
-        seq = FileSequence(
-            str_parts=seq_f1.seq_str_parts,
-            frames=tuple(ordered_frames),
-            fileobj=seq_f1.fileobj
-        )
-
-        sequences.append(seq)
-
-    return sequences
+    yield from build_sequences_concrete(sequences_f1)
 
 
 # TODO: add commandline formatter
 def format_sequence(seq: FileSequence) -> str:
+    """
+    Examples:
+        >>> from lss import SequenceStrParts
+        >>> from lss.dataclass.base import Fileobj
 
-    # TODO: Handle a sequence that is a single file!
+        >>> fo = Fileobj('root', '.ext')
 
-    count = len(seq.frames)
+        >>> str_parts = SequenceStrParts('mario01_', '.ext',  4)
+        >>> mario = FileSequence(str_parts, Fileobj('root', '.ext'), frames=(1, 2, 3))
+        >>> format_sequence(mario)
+        '3 mario01_%04d.ext 1-3'
+
+        >>> str_parts = SequenceStrParts('peach01', '.ext')
+        >>> peach = FileSequence(str_parts, fo)
+        >>> format_sequence(peach)
+        '1 peach01.ext'
+
+    :param seq:
+    :return:
+    """
     s = seq.str_parts
-    bn = f'{s.prefix}{s.printf}{s.suffix}'
 
-    # TODO: implement compact frame format
-    comp_frames = seq.frames
+    if seq.is_sequence:
+        count = len(seq.frames)
+        bn = f'{s.prefix}{s.printf}{s.suffix}'
 
-    return f'{count} {bn} {comp_frames}'
+        # TODO: implement compact frame format
+        comp_frames = seq.frames
+
+        return f'{count} {bn} {comp_frames}'
+
+    # This is a single file, i.e it dosent represent a sequence of files
+    else:
+        bn = seq.str_parts.prefix
+        return f'1 {bn}{s.suffix}'
 
 
 def run(dir_path):
@@ -95,6 +113,10 @@ def run(dir_path):
     Examples:
         >>> run('../../tests/files/simple1')
         5 file01_%04d.rgb 40-42, 44, 45
+        >>> run('../../tests/files/ex1')
+        1 elem.info
+        46 sd_fx29.%04d.rgb 101-121 123-147
+        1 strange.xml
 
     :param dir_path:
     :return:
