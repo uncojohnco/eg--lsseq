319b8dfeb794f862902ce78d77455dd4986a22e9
uncojohnco <johnny.p.cochrane@gmail.com>
2019-11-19 17:18:59 -0500

    run.py	tests - init

--- tests/run.py
+++ tests/run.py
@@ -3,8 +3,11 @@
 import os
 import sys
 import unittest
+import doctest
 
 if __name__ == '__main__':
+
     testsuite = unittest.TestLoader().discover(os.path.dirname(__file__))
     result = unittest.TextTestRunner(verbosity=1).run(testsuite)
+
     sys.exit(not result.wasSuccessful())
