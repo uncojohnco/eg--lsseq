2ec5673557082efc049c07c5f846e787ea3636cf
uncojohnco <johnny.p.cochrane@gmail.com>
2019-11-19 16:39:24 -0500

    setup.py	. setup.py	. - create a setup.py for installation and tests

---
+++ setup.py
@@ -0,0 +1,44 @@
+
+import os
+import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "src/lss/__version__.py")) as version_file:
+    exec(version_file.read())
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+descript = ('A personal project for implementing behaviour akin to`ls` in '
+            'addition to condensing sets of image sequences into a format '
+            'that is easier to visualise to the user from the command line')
+
+setuptools.setup(
+    name="lsseq-jc",
+    version=__version__,
+
+    author="Johnny Cochrane",
+    author_email="johnny.p.cochrane@gmail.com",
+
+    description=descript,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/uncojohnco/test-jc--dd-2019/",  # TODO: rename to "example-lsseq"
+
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+
+    python_requires='>=3.6',
+
+    test_suite="test.run",
+
+)
