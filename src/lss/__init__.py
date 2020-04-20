e97b7f53ba2e0d198ff4f698c1943a3aeb2d3844
uncojohnco <johnny.p.cochrane@gmail.com>
2020-04-20 13:10:59 -0400

    add logging null handler behavior for lib

--- src/lss/__init__.py
+++ src/lss/__init__.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
+import logging
+
 import lss.dataclass.base
 import lss.dataclass.item
 import lss.dataclass.sequence
+
+# https://realpython.com/python-logging-source-code/#library-vs-application-logging-what-is-nullhandler
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
 
 SubstrPos = lss.dataclass.base.SubstrPos
 SubstrMatch = lss.dataclass.base.SubstrMatch
